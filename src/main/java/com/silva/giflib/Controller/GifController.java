bd5bcd5415c43f04fae192438d5aeea0e6498058
Sergio Silva <ssilva@nearsoft.com>
2018-03-19 18:25:08 -0700

    Create an Index Page to Present a List of Data

--- src/main/java/com/silva/giflib/Controller/GifController.java
+++ src/main/java/com/silva/giflib/Controller/GifController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 public class GifController {
@@ -17,11 +18,12 @@ public class GifController {
     private GifRepository gifRepository;
 
     @RequestMapping("/")
-    public String listGifs() {
+    public String listGifs(ModelMap modelMap) {
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs", allGifs);
         return "home";
     }
 
-    // URL => http://localhost:8080/gif/android-explosion
     @RequestMapping("/gif/{name}")
     public String gifDetails(@PathVariable String name, ModelMap modelMap) {
         Gif gif = gifRepository.findByName(name);
